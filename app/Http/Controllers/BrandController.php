ad5d1a4f7366785523865cc409242b3dbcfe8018
LeGion <mehrdad.dadashi@gmail.com>
2024-02-24 19:38:26 +0300

    create brand

---
+++ app/Http/Controllers/BrandController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\BrandResource;
+use App\Models\Brand;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Spatie\FlareClient\Api;
+
+class BrandController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'display_name' => 'required|unique:brands',
+        ]);
+        if ($validator->fails()) {
+            return $this->errorResponse($validator->messages(), 422);
+        }
+
+        DB::beginTransaction();
+        $brand = Brand::create([
+            'name' => $request->name,
+            'display_name' => $request->name,
+        ]);
+        DB::commit();
+
+        return $this->successResponse(new BrandResource($brand), 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
