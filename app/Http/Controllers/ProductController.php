46a984370e33f9fcdd65ffd22b2556dd6ede4c7b
LeGion <mehrdad.dadashi@gmail.com>
2024-03-10 18:37:35 +0300

    update and delete product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\CategoryResource;
 use App\Http\Resources\ProductResource;
-use App\Models\Category;
 use App\Models\Product;
 use App\Models\ProductImage;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use JetBrains\PhpStorm\NoReturn;
 
 class ProductController extends ApiController
 {
@@ -24,7 +23,8 @@ class ProductController extends ApiController
             'meta' => ProductResource::collection($products)->response()->getData()->meta,
         ]);
     }
-    public function store(Request $request)
+
+    public function store(Request $request): \Illuminate\Http\JsonResponse
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string',
@@ -78,7 +78,76 @@ class ProductController extends ApiController
 
         return $this->successResponse(new ProductResource($product), 201);
     }
-    public function show(Product $product)
+
+    #[NoReturn] public function update(Request $request, Product $product): \Illuminate\Http\JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'brand_id' => 'required|exists:brands,id',
+            'category_id' => 'required|exists:categories,id',
+            'primary_image' => 'nullable|image',
+            'price' => 'integer',
+            'quantity' => 'integer',
+            'delivery_amount' => 'nullable|integer',
+            'description' => 'required',
+            'images.*' => 'nullable|image'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->errorResponse($validator->messages(), 422);
+        }
+        DB::beginTransaction();
+        if ($request->has('primary_image')) {
+            $primaryImageName = Carbon::now()->microsecond . '.' . $request->primary_image->extension();
+            $request->primary_image->storeAs('images/products', $primaryImageName, 'public');
+        }
+        if ($request->has('images')) {
+            $fileNameImages = [];
+            foreach ($request->images as $image) {
+                $fileNameImage = Carbon::now()->microsecond . '.' . $image->extension();
+                $image->storeAs('images/products', $fileNameImage, 'public');
+                array_push($fileNameImages, $fileNameImage);
+            }
+        }
+
+        $product->update([
+                'name' => $request->name,
+                'brand_id' => $request->brand_id,
+                'category_id' => $request->category_id,
+                'primary_image' => $request->has('primary_image') ? $primaryImageName : $product->primary_image,
+                'price' => $request->price,
+                'quantity' => $request->quantity,
+                'delivery_amount' => $request->delivery_amount,
+                'description' => $request->description,
+            ]);
+
+        if ($request->has('images')) {
+
+            foreach ($product->images as $productImage) {
+                $productImage->delete();
+            }
+            foreach ($fileNameImages as $fileNameImage) {
+                ProductImage::create([
+                    'product_id' => $product->id,
+                    'image' => $fileNameImage
+                ]);
+            }
+        }
+        DB::commit();
+
+        return $this->successResponse(new ProductResource($product));
+    }
+
+    public function destroy(Product $product): \Illuminate\Http\JsonResponse
+    {
+        DB::beginTransaction();
+        $product->delete();
+        DB::commit();
+
+        return $this->successResponse(new ProductResource($product));
+
+    }
+    public function show(Product $product): \Illuminate\Http\JsonResponse
     {
         return $this->successResponse(new ProductResource($product->load('images')));
     }
--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Resources;
 
-use App\Models\Product;
-use App\Models\ProductImage;
+
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
